cf3bea24ef0555f9924f4a9b422ad9fbd8b52929
srabondhr <srabandhr2@gmail.com>
2025-02-26 02:06:38 +0600

    homepage /product/profile /profile edit updated (s)

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -3,26 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <title>Edit Profile</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Edit Profile</h1>
-    <form method="post" action="{{ route('profile.update') }}">
-        @csrf
-        @method('PUT')
-        <label for="name">Name:</label>
-        <input type="text" id="name" name="name" value="{{ old('name', $user->name) }}" required>
-        <br>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" value="{{ old('email', $user->email) }}" required>
-        <br>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" value="{{ old('password') }}">
-        <br>
-        <label for="password_confirmation">Confirm Password:</label>
-        <input type="password" id="password_confirmation" name="password_confirmation" value="{{ old('password_confirmation') }}">
-        <br>
-        <button type="submit">Save Changes</button>
-    </form>
-    <a href="{{ route('profile.view') }}">Cancel</a>
+    <div class="container mt-5">
+        <h1 class="mb-4">Edit Profile</h1>
+        <form method="post" action="{{ route('profile.update') }}">
+            @csrf
+            @method('PUT')
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" class="form-control" value="{{ old('name', $user->name) }}" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" class="form-control" value="{{ old('email', $user->email) }}" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Password:</label>
+                <input type="password" id="password" name="password" class="form-control" value="{{ old('password') }}">
+            </div>
+            <div class="form-group">
+                <label for="password_confirmation">Confirm Password:</label>
+                <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+            </div>
+            <button type="submit" class="btn btn-primary">Save Changes</button>
+            <a href="{{ route('profile.view') }}" class="btn btn-secondary">Cancel</a>
+        </form>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
--- resources/views/profile/view.blade.php
+++ resources/views/profile/view.blade.php
@@ -3,11 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <title>Profile View</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Profile</h1>
-    <p>Name: {{ $user->name }}</p>
-    <p>Email: {{ $user->email }}</p>
-    <a href="{{ route('profile.edit') }}">Edit Profile</a>
+    <div class="container mt-5">
+        <h1 class="mb-4">Profile</h1>
+        <div class="card">
+            <div class="card-body">
+                <p><strong>Name:</strong> {{ $user->name }}</p>
+                <p><strong>Email:</strong> {{ $user->email }}</p>
+                <a href="{{ route('profile.edit') }}" class="btn btn-primary">Edit Profile</a>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
